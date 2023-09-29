70455d01ef3cc35677286ca30671ed66ed70e2e2
thien.vuong <thien.vuong@nois.vn>
2023-09-30 02:08:12 +0700

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -83,71 +83,73 @@ const Navbar = () => {
 
   return (
     <div>
-      {/* Check box */}
-      <div className='title'>
-        Position Card :
-      </div>
-      <div className="cont-main">
-        <Checkbox name="posCard" id="posCard-0" info="Left" onChange={changePosCard} />
-        <Checkbox name="posCard" id="posCard-1" info="Center" onChange={changePosCard} />
-        <Checkbox name="posCard" id="posCard-2" info="Right" onChange={changePosCard} />
-      </div>
-      <div className='title'>
-        Show Image :
-      </div>
-      <div className="cont-main">
-        <Checkbox name="showImg" id="showImg-0" info="Hide" onChange={changeShowImg} />
-        <Checkbox name="showImg" id="showImg-1" info="Show" onChange={changeShowImg} />
-      </div>
-      <div className='title'>
-        Sample Image :
-      </div>
-      <div className="cont-main">
-        <Checkbox name="sampleImg" id="sampleImg-0" info="Image 1" onChange={changeSampleImg} />
-        <Checkbox name="sampleImg" id="sampleImg-1" info="Image 2" onChange={changeSampleImg} />
-        <Checkbox name="sampleImg" id="sampleImg-2" info="Image 3" onChange={changeSampleImg} />
-        <Checkbox name="sampleImg" id="sampleImg-3" info="Image 4" onChange={changeSampleImg} />
-      </div>
-      <div className='title'>
-        Position Image :
-      </div>
-      <div className="cont-main">
-        <Checkbox name="posImg" id="posImg-0" info="Top" onChange={changePosImg} />
-        <Checkbox name="posImg" id="posImg-1" info="Right" onChange={changePosImg} />
-        <Checkbox name="posImg" id="posImg-2" info="Bottom" onChange={changePosImg} />
-        <Checkbox name="posImg" id="posImg-3" info="Left" onChange={changePosImg} />
-      </div>
-      <div className='title'>
-        Style Image :
-      </div>
-      <div className="cont-main">
-        <Checkbox name="styleImg" id="styleImg-0" info="Fit" onChange={changeStyleImg} />
-        <Checkbox name="styleImg" id="styleImg-1" info="Fill" onChange={changeStyleImg} />
-        <Checkbox name="styleImg" id="styleImg-2" info="Full" onChange={changeStyleImg} />
-        <Checkbox name="styleImg" id="styleImg-3" info="Over" onChange={changeStyleImg} />
-      </div>
-      <div className='title'>
-        Show Description :
-      </div>
-      <div className="cont-main">
-        <Checkbox name="showDesc" id="showDesc-0" info="Hide" onChange={changeShowDesc} />
-        <Checkbox name="showDesc" id="showDesc-1" info="Show" onChange={changeShowDesc} />
-      </div>
-      <div className='title'>
-        Show List :
-      </div>
-      <div className="cont-main">
-        <Checkbox name="showList" id="showList-0" info="Hide" onChange={changeShowList} />
-        <Checkbox name="showList" id="showList-1" info="Show" onChange={changeShowList} />
-      </div>
-      <div className='title'>
-        Show Button Action :
-      </div>
-      <div className="cont-main">
-        <Checkbox name="showBtn" id="showBtn-0" info="Hide" onChange={changeShowBtn} />
-        <Checkbox name="showBtn" id="showBtn-1" info="Link" onChange={changeShowBtn} />
-        <Checkbox name="showBtn" id="showBtn-2" info="Button" onChange={changeShowBtn} />
-        {/* <Checkbox name="showBtn" id="showBtn-3" info="Icon" onChange={changeShowBtn} /> */}
+      <div className='navbar'>
+        {/* Check box */}
+        <div className='title'>
+          Position Card :
+        </div>
+        <div className="cont-main">
+          <Checkbox name="posCard" id="posCard-0" info="Left" onChange={changePosCard} />
+          <Checkbox name="posCard" id="posCard-1" info="Center" onChange={changePosCard} />
+          <Checkbox name="posCard" id="posCard-2" info="Right" onChange={changePosCard} />
+        </div>
+        <div className='title'>
+          Show Image :
+        </div>
+        <div className="cont-main">
+          <Checkbox name="showImg" id="showImg-0" info="Hide" onChange={changeShowImg} />
+          <Checkbox name="showImg" id="showImg-1" info="Show" onChange={changeShowImg} />
+        </div>
+        <div className='title'>
+          Sample Image :
+        </div>
+        <div className="cont-main">
+          <Checkbox name="sampleImg" id="sampleImg-0" info="Image 1" onChange={changeSampleImg} />
+          <Checkbox name="sampleImg" id="sampleImg-1" info="Image 2" onChange={changeSampleImg} />
+          <Checkbox name="sampleImg" id="sampleImg-2" info="Image 3" onChange={changeSampleImg} />
+          <Checkbox name="sampleImg" id="sampleImg-3" info="Image 4" onChange={changeSampleImg} />
+        </div>
+        <div className='title'>
+          Position Image :
+        </div>
+        <div className="cont-main">
+          <Checkbox name="posImg" id="posImg-0" info="Top" onChange={changePosImg} />
+          <Checkbox name="posImg" id="posImg-1" info="Right" onChange={changePosImg} />
+          <Checkbox name="posImg" id="posImg-2" info="Bottom" onChange={changePosImg} />
+          <Checkbox name="posImg" id="posImg-3" info="Left" onChange={changePosImg} />
+        </div>
+        <div className='title'>
+          Style Image :
+        </div>
+        <div className="cont-main">
+          <Checkbox name="styleImg" id="styleImg-0" info="Fit" onChange={changeStyleImg} />
+          <Checkbox name="styleImg" id="styleImg-1" info="Fill" onChange={changeStyleImg} />
+          <Checkbox name="styleImg" id="styleImg-2" info="Full" onChange={changeStyleImg} />
+          <Checkbox name="styleImg" id="styleImg-3" info="Over" onChange={changeStyleImg} />
+        </div>
+        <div className='title'>
+          Show Description :
+        </div>
+        <div className="cont-main">
+          <Checkbox name="showDesc" id="showDesc-0" info="Hide" onChange={changeShowDesc} />
+          <Checkbox name="showDesc" id="showDesc-1" info="Show" onChange={changeShowDesc} />
+        </div>
+        <div className='title'>
+          Show List :
+        </div>
+        <div className="cont-main">
+          <Checkbox name="showList" id="showList-0" info="Hide" onChange={changeShowList} />
+          <Checkbox name="showList" id="showList-1" info="Show" onChange={changeShowList} />
+        </div>
+        <div className='title'>
+          Show Button Action :
+        </div>
+        <div className="cont-main">
+          <Checkbox name="showBtn" id="showBtn-0" info="Hide" onChange={changeShowBtn} />
+          <Checkbox name="showBtn" id="showBtn-1" info="Link" onChange={changeShowBtn} />
+          <Checkbox name="showBtn" id="showBtn-2" info="Button" onChange={changeShowBtn} />
+          {/* <Checkbox name="showBtn" id="showBtn-3" info="Icon" onChange={changeShowBtn} /> */}
+        </div>
       </div>
 
       <div className='cont-space'></div>
